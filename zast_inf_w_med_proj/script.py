fba3d3b63bd0b4c28d5a078bc1e1e42e4da24b4c
jgrycz <jaroslaw.grycz@gmail.com>
2016-05-24 16:45:25 +0200

    Added filtering specified values

--- zast_inf_w_med_proj/script.py
+++ zast_inf_w_med_proj/script.py
@@ -7,3 +7,8 @@ converters = {i: lambda s: 0 if s == 'no' else 1 for i in range(0, 8)}
 converters[0] = lambda n: float(n.replace(",", "."))
 
 array = np.loadtxt(file_path, delimiter="\t", converters=converters)
+inflammation = array[(array[:,6] == 1)]
+nephritis = array[(array[:,7] == 1)]
+healthy = array[(array[:,7] == 0) & (array[:,6] == 0)]
+print np.average(inflammation, axis=0)
+print np.average(nephritis, axis=0)
