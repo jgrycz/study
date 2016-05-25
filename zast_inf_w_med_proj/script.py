10b5ce40ed2648b1a6765a5e192e2aec2986aca5
jgrycz <jaroslaw.grycz@gmail.com>
2016-05-25 12:27:42 +0200

    added printing

--- zast_inf_w_med_proj/script.py
+++ zast_inf_w_med_proj/script.py
@@ -1,3 +1,4 @@
+from pprint import pprint as pp
 import numpy as np
 
 
@@ -7,8 +8,22 @@ converters = {i: lambda s: 0 if s == 'no' else 1 for i in range(0, 8)}
 converters[0] = lambda n: float(n.replace(",", "."))
 
 array = np.loadtxt(file_path, delimiter="\t", converters=converters)
+
 inflammation = array[(array[:,6] == 1)]
 nephritis = array[(array[:,7] == 1)]
 healthy = array[(array[:,7] == 0) & (array[:,6] == 0)]
-print np.average(inflammation, axis=0)
-print np.average(nephritis, axis=0)
+avg_inflammation = np.average(inflammation, axis=0)
+avg_nephritis = np.average(nephritis, axis=0)
+
+distance_inflammation = np.array([np.linalg.norm(row - avg_inflammation) for row in inflammation])
+distance_nephritis = np.array([np.linalg.norm(row - avg_nephritis) for row in nephritis])
+distance_helthy_ne = np.array([np.linalg.norm(row - avg_nephritis) for row in healthy])
+distance_helthy_in = np.array([np.linalg.norm(row - avg_inflammation) for row in healthy])
+
+print "\n".join("{} - {}".format(d, i) for i, d in zip(inflammation, distance_inflammation))
+print "\n\n"
+print "\n".join("{} - {}".format(d, i) for i, d in zip(nephritis, distance_nephritis))
+print "\n\n"
+print "\n".join("{} - {}".format(d, i) for i, d in zip(healthy, distance_helthy_ne))
+print "\n\n"
+print "\n".join("{} - {}".format(d, i) for i, d in zip(healthy, distance_helthy_in))
