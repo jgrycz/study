3dcb5ff49474aa17e51a21aa7803429133c6e251
jgrycz <jaroslaw.grycz@gmail.com>
2020-04-29 22:43:58 +0200

    Added base form

--- flask/helloworld/hello.py
+++ flask/helloworld/hello.py
@@ -1,12 +1,22 @@
-from flask import Flask, request, make_response, redirect, abort, render_template
+from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "heheheh"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField("Jak masz na imię?", validators=[DataRequired()])
+    submit = SubmitField("Wyślij")
 
 
 @app.errorhandler(404)
@@ -19,9 +29,17 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Wyglada na to że teraz nazywasz sie inaczej")
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
