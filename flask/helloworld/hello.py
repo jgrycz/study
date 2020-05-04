d758a9e717bca6a6446770fbdf6ca39ca9cfffd4
jgrycz <jaroslaw.grycz@gmail.com>
2020-05-04 23:15:21 +0200

    Added database

--- flask/helloworld/hello.py
+++ flask/helloworld/hello.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from flask_moment import Moment
 
 from wtforms import StringField, SubmitField
@@ -8,10 +10,38 @@ from flask_wtf import FlaskForm
 
 from datetime import datetime
 
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "heheheh"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+migrate = Migrate(app, db)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
 
 
 class NameForm(FlaskForm):
@@ -34,14 +64,28 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get("name")
-        if old_name is not None and old_name != form.name.data:
-            flash("Wyglada na to że teraz nazywasz sie inaczej")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            flash("Dodano nowego użytkownika: {}".format(user.username))
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template("index.html", form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template("index.html",
+        form=form, name=session.get('name'),
+        known=session.get('known', False), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def name(name):
     return render_template('user.html', name=name)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
