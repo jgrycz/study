811996afbaff9b68ff8922c9de5c81e71b8c78eb
jgrycz <jaroslaw.grycz@gmail.com>
2020-06-27 23:40:05 +0200

    Added registration form

--- flask/helloworld/app/auth/forms.py
+++ flask/helloworld/app/auth/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
-from wtfforms import ValidationError
 from ..models import User
 
 
@@ -9,14 +8,23 @@ class LoginForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Zaloguj')
+    submit = SubmitField('Login')
 
 
 class RegistrationForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
         'Username can contain only allowed characers')])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', 'Passwords have to be equal')])
     password2 = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Zaloguj')
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email address is already in use')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username is already in use')
+
--- flask/helloworld/app/auth/views.py
+++ flask/helloworld/app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -25,4 +26,58 @@ def login():
 def logout():
     logout_user()
     flash("You were log out")
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        url = url_for('auth.confirm', token=token, _external=True)
+        flash('Before you log in activate your account please, clicking following url: {}'.format(url))
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+
+@auth.route('confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('Your account has been confirmed')
+    else:
+        flash('Confirmation email is incorrect or has expired')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('confirm')
+@login_required
+def resend_information():
+    token = current_user.generate_confirmation_token()
+    url = url_for('auth.confirm', token=token, _external=True)
+    flash('Before you log in activate your account please, clicking following url: {}'.format(url))
     return redirect(url_for('main.index'))
